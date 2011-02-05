be7ecccc53a106f495840730f1bf639102cc2c77
Fernando Blat <ferblape@gmail.com>
2011-02-05 18:16:13 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blathost::Application.config.secret_token = 'ee9897805f0e60b2fa93943b80aac0444a52992bc33cc95c6640b01eebe93c0bbf386fc1326faa0eac23d0b1bb89a90f886d616a0e2526999d73944455d24670'
