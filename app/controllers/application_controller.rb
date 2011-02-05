8cd540f8e3c035812cc2f35de44b93c1bc0dd96b
Fernando Blat <ferblape@gmail.com>
2011-02-05 18:42:36 +0100

    Ferro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  CONFIGURED_SITES = %W{ mariaysasi.com ferroea.com claquetitas.com goldenratio.me }
+
+  before_filter :set_site
+
+  protected
+
+  def set_site
+    unless CONFIGURED_SITES.include?(request.host)
+      render_404 and return false
+    else
+      if request.path == '/'
+        render :text => File.read("#{Rails.root}/public/#{request.host}/index.html") and return
+      else
+
+      end
+    end
+
+  end
+
+  def render_404
+    render :file => "public/404.html.erb", :status => 404, :layout => false
+  end
+
 end
